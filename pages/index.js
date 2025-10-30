abe313779e9a7eb87d61b885399dd229baf6b553
Luis Novoa <luis.matte.novoa@gmail.com>
2025-10-29 22:04:40 -0400

    Applies formatting to existing files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Oi, pai e mãe! Vocês são as pessoas mais importantes da minha e eu amo muito vocês</h1>
+  return (
+    <h1>
+      Oi, pai e mãe! Vocês são as pessoas mais importantes da minha e eu amo
+      muito vocês
+    </h1>
+  );
 }
+
+const test = () => {
+  console.log("hiiiiii");
+};
 
 export default Home;
